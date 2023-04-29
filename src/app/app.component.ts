d965cdcf414f9d38d53fd63d60fb4c67dcb59297
Arthur <nespoulous.arthur@yahoo.fr>
2023-04-29 17:00:05 +0200

    working template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,14 @@
 import { Component } from '@angular/core';
+import { 
+  Firestore,
+  collection,
+  addDoc,
+  collectionData,
+  doc,
+  updateDoc,
+  deleteDoc
+} from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +17,54 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ang-fire';
+  userData!: Observable<any>;
+  constructor(
+    private firestore: Firestore
+  ) {
+    this.getData();
+  }
+
+  addData(f: any) {
+    const collectionInstance = collection(this.firestore, 'users');
+    addDoc(collectionInstance, f.value)
+      .then(() => {
+        console.log("Success");
+      })
+      .catch((err) => {
+        console.log(err);
+      });    
+  }
+
+  getData() {
+    const collectionInstance = collection(this.firestore, 'users');
+    collectionData(collectionInstance, { idField: 'id' }).subscribe((val) => {
+      console.log(val);
+    })
+    this.userData = collectionData(collectionInstance, { idField: 'id' });
+  }
+
+  updateData(id: string) {
+    const docInstance = doc(this.firestore, 'users', id);
+    const updateData = {
+      name: 'updatedName'
+    }
+    updateDoc(docInstance, updateData)
+      .then(() => {
+        console.log("Updated");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  deleteData(id: string) {
+    const docInstance = doc(this.firestore, 'users', id);
+    deleteDoc(docInstance)
+      .then(() => {
+        console.log("Deleted");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 }
